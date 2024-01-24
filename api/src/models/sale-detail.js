53a9eea13402ced47b491a98d33e6e2ff5a8faf7
DiegoSehwerert <dsehwerert@gmail.com>
2024-01-24 10:35:49 +0100

    hechos todos los modelos hay que cambiar los separados por "-" en vez de "_"

---
+++ api/src/models/sale-detail.js
@@ -0,0 +1,86 @@
+module.exports = function (sequelize, DataTypes) {
+  const SaleDetail = sequelize.define('SaleDetail', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    saleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    localeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    priceId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    priceDiscountId: {
+      type: DataTypes.INTEGER,
+    },
+    taxId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    basePrice: {
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false
+    },
+    taxPrice: {
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      get () {
+        return this.getDataValue('createdAt')
+          ? this.getDataValue('createdAt').toISOString().split('T')[0]
+          : null
+      }
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      get () {
+        return this.getDataValue('updatedAt')
+          ? this.getDataValue('updatedAt').toISOString().split('T')[0]
+          : null
+      }
+    }
+  }, {
+    sequelize,
+    tableName: 'sale-details',
+    timestamps: true,
+    paranoid: true,
+    indexes: [
+      {
+        name: 'PRIMARY',
+        unique: true,
+        using: 'BTREE',
+        fields: [
+          { name: 'id' }
+        ]
+      }
+    ]
+  })
+
+  SaleDetail.associate = function (models) {
+
+  }
+
+  return SaleDetail
+}
