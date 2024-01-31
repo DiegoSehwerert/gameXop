e3cfa5304b4ac836a624392957c00898a754d7e0
DiegoSehwerert <you@example.com>
2024-01-31 15:19:42 +0100

    las tabs funcionan

--- api/src/routes/admin-product-category-realtion-routes.js
+++ api/src/routes/admin-product-category-realtion-routes.js
@@ -1,6 +1,6 @@
 module.exports = (app, upload) => {
   const router = require('express').Router()
-  const controller = require('../controllers/admin/product-category-realtion-controller.js')
+  const controller = require('../controllers/admin/product-category-relation-controller.js')
 
   router.post('/', controller.create)
   router.get('/', controller.findAll)
@@ -8,5 +8,5 @@ module.exports = (app, upload) => {
   router.put('/:id', controller.update)
   router.delete('/:id', controller.delete)
 
-  app.use('/api/admin/product-category-realtions', router)
+  app.use('/api/admin/product-category-relations', router)
 }
