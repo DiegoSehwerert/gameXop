7b42802a9fbba085611629bb0b1ce340a58e24ca
DiegoSehwerert <dsehwerert@gmail.com>
2024-02-13 19:49:02 +0100

    Actualizando codigos de errores en Todos los controladores

--- api/src/controllers/admin/contact-controller.js
+++ api/src/controllers/admin/contact-controller.js
@@ -6,9 +6,22 @@ exports.create = (req, res) => {
   Contact.create(req.body).then(data => {
     res.status(200).send(data)
   }).catch(err => {
-    res.status(500).send({
-      message: err.errors || 'Algún error ha surgido al insertar el dato.'
-    })
+    exports.create = (req, res) => {
+      Faq.create(req.body).then(data => {
+        res.status(200).send(data)
+      }).catch(err => {
+        console.log(err)
+        if (err.errors) {
+          res.status(422).send({
+            message: err.errors
+          })
+        } else {
+          res.status(500).send({
+            message: 'Algún error ha surgido al insertar el dato.'
+          })
+        }
+      })
+    }
   })
 }
 
